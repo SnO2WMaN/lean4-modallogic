a90abc0312dfc8b2d94026ac0fb2dcad0db85324
SnO2WMaN <me@sno2wman.net>
2023-10-05 11:17:33 +0900

    root

--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
--- import «ModalLogic»
+import «ModalLogic»
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
---
+++ ModalLogic.lean
@@ -0,0 +1,8 @@
+import ModalLogic.PropositionalLogic.DeductionSystem
+
+import ModalLogic.Arithmetic.IT1
+import ModalLogic.Arithmetic.IT2
+import ModalLogic.Arithmetic.Loeb_with_IT2
+import ModalLogic.Arithmetic.Loeb_without_IT2
+
+def hello := "world"
